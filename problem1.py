8d3434fded92c3e13f3090084ccd6d5dc4aabb88
jagonci2o <aj47073@mail.harpercollege.edu>
2025-01-15 13:07:25 -0600

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,9 @@
+#problem 1
+
+firstName = input("Please enter your first name: ")
+lastName = input("Please enter your last name: ")
+
+fullName = lastName + ", " + firstName
+
+print(fullName)
+
---
+++ problem5.py
@@ -0,0 +1,9 @@
+#problem 5
+
+word = "Hello, World!"
+floatNum = 3.14
+intNum = 42
+
+print(type(word))  
+print(type(floatNum))  
+print(type(intNum))
